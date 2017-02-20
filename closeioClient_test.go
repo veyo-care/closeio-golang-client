98ef227eeec51f7b1af64f9dca5114725f36a723
patrikeh <patrikeh@gmail.com>
2017-02-20 12:51:33 +0100

    fix closeio test

--- closeioClient_test.go
+++ closeioClient_test.go
@@ -5,6 +5,24 @@ import "testing"
 func Test_ConvertQuery(t *testing.T) {
 
 	query := map[string][]string{
+
+		"Custom.Field": []string{"One", "Two", "Three"},
+	}
+
+	queryString := convertQueryFields(query)
+
+	expected := `(Custom.Field:"One" OR Custom.Field:"Two" OR Custom.Field:"Three")`
+
+	if queryString != expected {
+		t.Errorf("Expected %s, got %s", expected, queryString)
+	}
+}
+
+// Test assumes golang maps are ordered, which they aren't
+
+/*func Test_ConvertQuery(t *testing.T) {
+
+	query := map[string][]string{
 		"Status":       []string{"Open", "Lost"},
 		"Town":         []string{"Paris"},
 		"Custom.Field": []string{"One", "Two", "Three"},
@@ -18,3 +36,4 @@ func Test_ConvertQuery(t *testing.T) {
 		t.Errorf("Expected %s, got %s", expected, queryString)
 	}
 }
+*/
