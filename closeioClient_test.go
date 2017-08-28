b368e882085827af89163a34ab93a5baabf584a5
cgcote <charles@veyo-care.com>
2017-08-28 16:08:13 +0200

    Modify the way custom fields are ser/deser

--- closeioClient_test.go
+++ closeioClient_test.go
@@ -1,6 +1,10 @@
 package closeio
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_ConvertQuery(t *testing.T) {
 
@@ -16,6 +20,24 @@ func Test_ConvertQuery(t *testing.T) {
 	if queryString != expected {
 		t.Errorf("Expected %s, got %s", expected, queryString)
 	}
+}
+
+func Test_UpdateCustomFields(t *testing.T) {
+
+	lead := &Lead{
+		Name:        "Test",
+		Description: "description",
+		Custom: map[string]interface{}{
+			"field1": "value1",
+			"field2": "value2",
+		},
+	}
+
+	content, err := LeadToJSON(*lead)
+	assert.Nil(t, err)
+	retrieved, err := JSONToLead(content)
+	assert.Nil(t, err)
+	assert.EqualValues(t, lead, retrieved)
 }
 
 // Test assumes golang maps are ordered, which they aren't
--- datamodel.go
+++ datamodel.go
@@ -1,6 +1,11 @@
 package closeio
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type Contact struct {
 	LeadID      string     `json:"lead_id,omitempty"`
@@ -179,11 +184,48 @@ type Attachment struct {
 	URL         string `json:"url,omitempty"`
 }
 
+func JSONToLead(ser []byte) (*Lead, error) {
+
+	var lead Lead
+	err := json.Unmarshal(ser, &lead)
+	if err != nil {
+		return nil, err
+	}
+
+	lead.Custom = make(map[string]interface{})
+	var keyValues map[string]interface{}
+	err = json.Unmarshal(ser, &keyValues)
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range keyValues {
+		if strings.HasPrefix(key, "custom.") {
+			lead.Custom[strings.Replace(key, "custom.", "", 1)] = value
+		}
+	}
+
+	return &lead, nil
+}
+
+func LeadToJSON(lead Lead) ([]byte, error) {
+
+	bytes, _ := json.Marshal(lead)
+	var dynamicFields map[string]interface{}
+	json.Unmarshal(bytes, &dynamicFields)
+
+	for key, value := range lead.Custom {
+		dynamicFields[fmt.Sprintf("custom.%s", key)] = value
+	}
+
+	return json.Marshal(dynamicFields)
+}
+
 type Lead struct {
 	Addresses      []Address              `json:"addresses,omitempty"`
 	Contacts       []Contact              `json:"contacts,omitempty"`
 	CreationDate   *time.Time             `json:"date_created,omitempty"`
-	Custom         map[string]interface{} `json:"custom,omitempty"`
+	Custom         map[string]interface{} `json:"-"`
 	Description    string                 `json:"description,omitempty"`
 	DisplayName    string                 `json:"display_name,omitempty"`
 	HtmlURL        string                 `json:"html_url,omitempty"`
