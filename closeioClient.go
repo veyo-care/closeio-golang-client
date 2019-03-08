3daedc59de240250c40da5bb247bf62ae75ef006
Eric Engoron <eric.engoron@gmail.com>
2019-03-08 14:51:47 -0500

    Change base url to api.close.com

--- closeioClient.go
+++ closeioClient.go
@@ -627,7 +627,7 @@ func (c HttpCloseIoClient) UpdateAddress(address Address, leadID string) error {
 }
 
 func (c HttpCloseIoClient) getResponse(method, route string, query map[string]string, body io.Reader) ([]byte, error) {
-	req, err := http.NewRequest(method, fmt.Sprintf("https://app.close.io/api/v1/%s/", route), body)
+	req, err := http.NewRequest(method, fmt.Sprintf("https://api.close.com/api/v1/%s/", route), body)
 
 	if err != nil {
 		return nil, fmt.Errorf("Error while creating http request %s", err.Error())
